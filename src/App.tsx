40ce16d5971d7ffbc254909fdc4ccc4aa7f5aee1
sudhavikrama <projectworkdemo2020@gmail.com>
2024-12-06 14:32:49 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Chatbot6 from './Demo';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Chatbot6/>
+    </div>
+  );
+}
+
+export default App;
